8e705475333a8b1566f433ac112d60af61f6b23c
= <=>
2018-01-18 00:04:26 -0200

    Criacao da classe copy-file.

---
+++ copy-file.py
@@ -0,0 +1,63 @@
+from shutil import copyfile
+import csv
+import os
+import string
+
+class CopyFile(object):
+    def __init__(self):
+        # Define o caminho onde o dataset está.
+        self.PATH = '/media/sf_paulo/RNP/dataset/X-ray-chest'
+        self.data_path = self.PATH + '/images_001'
+        self.data_dir_list = os.listdir(self.data_path)
+
+        self.imagens = []
+        self.classes = []
+
+    def open_CSV(self):
+        # Abre arquivo csv com o nome das imagens e suas respectivas classes.
+        with open('/media/sf_paulo/RNP/dataset/X-ray-chest/image-class.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            i = 0
+            # define um dicionario para armazenar o nome das imagens e as suas respectivas classes do arquivo csv.
+            # imagens_classes = {}
+
+            # Laço que percorre os registros do arquivo csv e seta as listas de imagens e classes.
+            for row in reader:
+                imagem = row['Image Index']
+                classe = row['Finding Labels']
+
+                self.imagens.append(imagem)
+                self.classes.append(classe)
+
+    def copy_file(self):
+        #self.classe = ""
+        classe = 'teste'
+        for dataset in self.data_dir_list:
+
+            # Carrega as imagens do diretório.
+            img_list = os.listdir(self.data_path + '/' + dataset)
+            # print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
+            # Percorre a lista de imagens para processá-las e adicioná-las à lista de imagens (img_data_list).
+            for img in img_list:
+                src = self.data_path + '/' + dataset + '/' + img
+
+                # Recupera o índice da imagem na lista de imagens.
+                image_index = self.imagens.index(img)
+                # Recupera a classe correspondente à imagem recuperada acima.
+                self.classe = self.classes[image_index]
+
+                classe.replace("|", "-")
+                #Monta o diretorio onde será gravada a imagem (com o nome da classe).
+                dst_dir = self.data_path + '/' + dataset + '/' + classe
+                #Monta o diretório e o nome de destino da imagem.
+                dst = self.data_path + '/' + dataset + '/' + classe + '/' + img
+
+                if not os.path.exists(dst_dir):
+                    os.makedirs(dst_dir)
+
+                copyfile(src, dst)
+
+
+x = CopyFile()
+x.open_CSV()
+x.copy_file()
